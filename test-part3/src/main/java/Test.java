95ac6323474c147197c050d7e75806d2679a9df4
wanfeng <wanfengiii@163.com>
2022-09-28 10:11:23 +0800

    init

---
+++ test-part3/src/main/java/Test.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+public class Test {
+    private static String A = "a";
+    private static String B = "b";
+    private static String D = "d";
+    private static String S = "s";
+    private static String F = "f";
+    // dddd
+    // test
+    // aaa
+    // bbb
+    // test
+
+    public static void testA() {
+        System.out.println(A);
+    }
+}
---
+++ test-part3/src/main/java/aa/Test2.java
@@ -0,0 +1,10 @@
+package src.main.java.aa;
+
+public class Test2 {
+    private static String A = "a";
+    private static String B = "b";
+    private static String D = "d";
+    private static String S = "s";
+    private static String F = "f";
+    private static String F1 = "f1";
+}
